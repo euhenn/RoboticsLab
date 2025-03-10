394cbd712f6ee83696128f2a95f9c01eeecd9956
Enrico Defend <enricod2001@gmail.com>
2025-03-10 10:35:57 +0100

    first funtion of 2 (not 2.1) - forking

---
+++ NA1 code/script_2.m
@@ -0,0 +1,19 @@
+clear all;
+close all;
+addpath(genpath('utils'));
+
+%% Set simulation parameters
+r = 0.03;
+d = 0.165;
+T_SIMULATION = 25;
+Q_INITIAL = [0;0;0];
+
+%% Set input
+OMEGA_L = 1;
+OMEGA_R = 1;
+
+%%
+x = linspace(0,10,1000);
+y = logspace(0,20,1000);
+
+[q,input] = cartesian_output_2_kin_stateInput(x,y,T_SIMULATION);
