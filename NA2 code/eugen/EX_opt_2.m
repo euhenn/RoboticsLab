75df0ce3d9b21eccee5650bc6ef1b699bd44a1a2
Eugen <eugenhulub@gmail.com>
2025-05-10 21:47:41 +0200

    Opt NA2 done

--- NA2 code/eugen/EX_opt_2.m
+++ NA2 code/eugen/EX_opt_2.m
@@ -1,8 +1,8 @@
 clear all; close all; clc;
 
 %% Load data
-load('data/ekf_001.mat');
-values = out.ekf_001.signals.values;
+load('data/ekf_dist_001.mat');
+values = out.data.signals.values;
 x_actual = squeeze(values(:,1));
 y_actual = squeeze(values(:,2));
 x_ekf    = squeeze(values(:,3));
@@ -14,14 +14,34 @@ plot(x_actual, y_actual, 'b-', 'LineWidth', 1.5); hold on;
 plot(x_ekf, y_ekf, 'ro', 'MarkerSize', 3, 'MarkerFaceColor', 'r');
 grid on;
 
-title('Actual vs EKF, p loss = 0.01');
+title('Actual vs EKF, var = 0.01');
+legend('Actual position','EKF estimated position');
+xlabel('X [m]');
+ylabel('Y [m]');
+
+
+%% Load data
+load('data/ekf_dist_0001.mat');
+values = out.data.signals.values;
+x_actual = squeeze(values(:,1));
+y_actual = squeeze(values(:,2));
+x_ekf    = squeeze(values(:,3));
+y_ekf    = squeeze(values(:,4));
+
+%% Plot
+figure;
+plot(x_actual, y_actual, 'b-', 'LineWidth', 1.5); hold on;
+plot(x_ekf, y_ekf, 'ro', 'MarkerSize', 3, 'MarkerFaceColor', 'r');
+grid on;
+
+title('Actual vs EKF, var = 0.001');
 legend('Actual position','EKF estimated position');
 xlabel('X [m]');
 ylabel('Y [m]');
 
 %% Load data
-load('data/ekf_090.mat');
-values = out.ekf_090.signals.values;
+load('data/ekf_dist_00001.mat');
+values = out.data.signals.values;
 x_actual = squeeze(values(:,1));
 y_actual = squeeze(values(:,2));
 x_ekf    = squeeze(values(:,3));
@@ -33,26 +53,7 @@ plot(x_actual, y_actual, 'b-', 'LineWidth', 1.5); hold on;
 plot(x_ekf, y_ekf, 'ro', 'MarkerSize', 3, 'MarkerFaceColor', 'r');
 grid on;
 
-title('Actual vs EKF, p loss = 0.90');
-legend('Actual position','EKF estimated position');
-xlabel('X [m]');
-ylabel('Y [m]');
-
-%% Load data
-load('data/ekf_099.mat');
-values = out.ekf_099.signals.values;
-x_actual = squeeze(values(:,1));
-y_actual = squeeze(values(:,2));
-x_ekf    = squeeze(values(:,3));
-y_ekf    = squeeze(values(:,4));
-
-%% Plot
-figure;
-plot(x_actual, y_actual, 'b-', 'LineWidth', 1.5); hold on;
-plot(x_ekf, y_ekf, 'ro', 'MarkerSize', 3, 'MarkerFaceColor', 'r');
-grid on;
-
-title('Actual vs EKF, p loss = 0.99');
+title('Actual vs EKF, var = 0.0001');
 legend('Actual position','EKF estimated position');
 xlabel('X [m]');
 ylabel('Y [m]');
